6c4d978e57b020c241cb6ea970a75dcd988830c6
luis eduardo frias <luiseduardofrias27@gmail.com>
2024-07-29 00:02:41 -0400

    test3

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   env: {
-    NEXT_PUBLIC_BASE_API_TECHCELLXPRESS:"https://api-techcellxpress-4addxp125-luiseduardofrias-projects.vercel.app/api-docs/",
+    NEXT_PUBLIC_BASE_API_TECHCELLXPRESS:"https://api-techcellxpress-4addxp125-luiseduardofrias-projects.vercel.app/",
     NEXT_PUBLIC_SECRET_JWT_KEY: "3$t0EsUnC0d1guBonoS3cr3t0_p@r@-t0d0L4@p"
   },
 }
